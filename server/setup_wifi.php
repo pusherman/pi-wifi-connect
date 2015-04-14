b06782e0b6efb895e7787264204a09c6125daa42
Corey Wilson <phpuser@gmail.com>
2015-04-13 21:35:04 -0400

    added simple server config writer

---
+++ server/setup_wifi.php
@@ -0,0 +1,20 @@
+<?php
+
+$config = 'wpa.conf';
+
+if (file_exists($config)) {
+  unlink($config);
+}
+
+$settings = 'network={
+  ssid="' . $_GET['ssid'] . '"
+  proto=RSN
+  key_mgmt=WPA-PSK
+  pairwise=CCMP TKIP
+  group=CCMP TKIP
+  psk="' . $_GET['psk'] . '"
+}';
+
+file_put_contents($config, $settings);
+
+print 'super!';
